a885f519e4d7b7c95f46415b8e53fc38d646da24
Muhammad Hamza <hamza.muhammad9181@gmail.com>
2026-06-06 17:01:00 +0500

    number

--- src/app/components/footer/footer.ts
+++ src/app/components/footer/footer.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { NgFor } from '@angular/common';
+import { AdminService } from '../../services/admin.service';
 
 @Component({
   selector: 'app-footer',
-  imports: [RouterLink],
+  imports: [RouterLink, NgFor],
   templateUrl: './footer.html',
   styleUrl: './footer.css'
 })
-export class Footer {
+export class Footer implements OnInit {
   year = new Date().getFullYear();
+  spaceTypes = signal<string[]>([]);
+
+  constructor(private adminService: AdminService) {}
+
+  ngOnInit() {
+    this.adminService.getSpaceTypes(1, 200).subscribe({
+      next: (res) => {
+        const items = Array.isArray(res) ? res : (res?.data as any[] ?? []);
+        this.spaceTypes.set(items.map((t: any) => t.name).filter(Boolean));
+      }
+    });
+  }
 }
